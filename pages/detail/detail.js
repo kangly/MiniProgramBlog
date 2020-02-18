08e3f9ec82c88f1904f6f56cf66a27b422a3e299
371976974@qq.com <lovem922419.com>
2020-02-18 17:08:26 +0800

    Initial Commit

---
+++ pages/detail/detail.js
@@ -0,0 +1,95 @@
+// pages/detail/detail.js
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    article: {},
+    info: ''
+  },
+  loadArticle: function () {
+    var that = this
+    wx.showLoading({
+      title: '加载中'
+    })
+    wx.request({
+      url: `https://kangly.club/api/article/${that.data.article.id}`,
+      success: (res) => {
+        that.setData({
+          article: {
+            title: res.data.data.title,
+            image: res.data.data.image,
+            author: res.data.data.author,
+            content: res.data.data.content,
+            posted_at: res.data.data.posted_at,
+            views: res.data.data.views,
+            votes: res.data.data.votes
+          }
+        })
+        // 引入 wxParse 组件处理文章正文
+        var wxParse = require('../components/wxParse/wxParse.js')
+        wxParse.wxParse('article_content', 'html', that.data.article.content, that, 1)
+      },
+      fail: function () {
+        that.data.info = '获取详情数据失败'
+      },
+      complete: function () {
+        wx.hideLoading()
+      }
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.data.article.id = options.id
+    this.loadArticle()
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    let id = this.data.article.id
+    let title = this.data.article.title
+    return {
+      title: `小康博客 - ${title}`,
+      path: `/pages/detail/detail?id=${id}`
+    }
+  },
+})
---
+++ pages/index/index.js
@@ -0,0 +1,70 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    articles: [],
+    isLoadingMore: false,
+    currentPage: 1,
+    info: ''
+  },
+  loadArticles: function () {
+    var that = this
+    wx.request({
+      url: `https://kangly.club/api/articles?page=${that.data.currentPage}`,
+      success: (res) => {
+        if (res.data.message === 'success') {
+          if (res.data.articles.length == 0) {
+            that.setData({
+              isLoadingMore: false,
+              info: '我是有底线的'
+            });
+          }
+          that.setData({
+            articles: that.data.articles.concat(res.data.articles)
+          })
+        } else {
+          that.setData({
+            info: '列表加载失败，请重试'
+          })
+        }
+        wx.hideLoading()
+      }
+    })
+  },
+  onReachBottom: function () {
+    this.data.currentPage++
+    if (this.data.isLoadingMore && this.data.currentPage > 10) {
+      // 最多加载10页
+      this.data.isLoadingMore = false
+      this.data.info = '我是有底线的'
+      return
+    }
+    this.data.isLoadingMore = true
+    this.loadArticles()
+  },
+  onShareAppMessage() {
+    return {
+      title: '小康博客',
+      path: '/pages/index'
+    }
+  },
+  postDetail: function (event) {
+    wx.navigateTo({
+      url: '/pages/detail/detail?id=' + event.currentTarget.dataset.id,
+    })
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    wx.showLoading({
+      title: '加载中...'
+    })
+    this.loadArticles()
+  }
+})
