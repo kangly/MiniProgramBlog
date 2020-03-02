ab1f2876d718fdc9d62f632b0f5beab693508921
371976974@qq.com <lovem922419.com>
2020-03-02 15:28:26 +0800

    修复富文本解析组件无法解析code标签导致代码块无法正常显示的bug

--- components/wxParse/wxDiscode.js
+++ components/wxParse/wxDiscode.js
@@ -179,7 +179,7 @@ function strMoreDiscode(str){
     str = str.replace(/\r\n/g,"");  
     str = str.replace(/\n/g,"");
 
-    str = str.replace(/code/g,"wxxxcode-style");
+    str = str.replace(/code/g,"code-style");
     return str;
 }
 
