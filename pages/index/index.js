84065c43f8d45fc4b529af5b19e8b0a5b7945f23
371976974@qq.com <lovem922419.com>
2020-03-03 15:46:38 +0800

    修复富文本解析组件code标签样式、增加首页推荐轮播

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,12 +5,14 @@ const app = getApp()
 Page({
   data: {
     articles: [],
+    swiperList: [],
     isLoadingMore: false,
     currentPage: 1,
     info: '',
-    keywords: ''
+    keywords: '',
+    cardCur: 0,
+    openid: app.globalData.openid
   },
-
   /**
    * 页面上拉触底事件的处理函数
    */
@@ -37,7 +39,20 @@ Page({
       path: '/pages/index/index'
     }
   },
-
+  loadArticleRecommends: function () {
+    var that = this
+    wx.request({
+      url: `https://kangly.club/api/article/recommend`,
+      success: (res) => {
+        if (res.data.message === 'success') {
+          that.setData({
+            swiperList: res.data.articles
+          })
+        }
+        wx.hideLoading()
+      }
+    })
+  },
   loadArticles: function () {
     var that = this
     wx.request({
@@ -69,13 +84,11 @@ Page({
       }
     })
   },
-
   postDetail: function (event) {
     wx.navigateTo({
       url: '/pages/detail/detail?id=' + event.currentTarget.dataset.id,
     })
   },
-
   wxSearchInput: function (e) {
     if (this.data.keywords){
       wx.navigateTo({
@@ -89,7 +102,6 @@ Page({
       })
     }
   },
-
   wxSearchButton: function (e) {
     this.setData({
       keywords: e.detail.value
@@ -108,6 +120,70 @@ Page({
       title: '加载中...'
     })
     this.loadArticles();
+    this.loadArticleRecommends();
+    this.towerSwiper('swiperList');
+    // 初始化towerSwiper 传已有的数组名即可
+  },
+  
+  // cardSwiper
+  cardSwiper(e) {
+    this.setData({
+      cardCur: e.detail.current
+    })
+  },
+  // towerSwiper
+  // 初始化towerSwiper
+  towerSwiper(name) {
+    let list = this.data[name];
+    for (let i = 0; i < list.length; i++) {
+      list[i].zIndex = parseInt(list.length / 2) + 1 - Math.abs(i - parseInt(list.length / 2))
+      list[i].mLeft = i - parseInt(list.length / 2)
+    }
+    this.setData({
+      swiperList: list
+    })
+  },
+  // towerSwiper触摸开始
+  towerStart(e) {
+    this.setData({
+      towerStart: e.touches[0].pageX
+    })
+  },
+  // towerSwiper计算方向
+  towerMove(e) {
+    this.setData({
+      direction: e.touches[0].pageX - this.data.towerStart > 0 ? 'right' : 'left'
+    })
+  },
+  // towerSwiper计算滚动
+  towerEnd(e) {
+    let direction = this.data.direction;
+    let list = this.data.swiperList;
+    if (direction == 'right') {
+      let mLeft = list[0].mLeft;
+      let zIndex = list[0].zIndex;
+      for (let i = 1; i < list.length; i++) {
+        list[i - 1].mLeft = list[i].mLeft
+        list[i - 1].zIndex = list[i].zIndex
+      }
+      list[list.length - 1].mLeft = mLeft;
+      list[list.length - 1].zIndex = zIndex;
+      this.setData({
+        swiperList: list
+      })
+    } else {
+      let mLeft = list[list.length - 1].mLeft;
+      let zIndex = list[list.length - 1].zIndex;
+      for (let i = list.length - 1; i > 0; i--) {
+        list[i].mLeft = list[i - 1].mLeft
+        list[i].zIndex = list[i - 1].zIndex
+      }
+      list[0].mLeft = mLeft;
+      list[0].zIndex = zIndex;
+      this.setData({
+        swiperList: list
+      })
+    }
   },
   
   /**
